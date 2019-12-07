20c6f2b8bbc7e3abe0547a232f847b41eb2f98c9
Linmiao Xu <linmiao.xu@gmail.com>
2019-12-06 20:20:24 -0500

    Enable skipping regenerating existing tiles

--- generate_tiles.py
+++ generate_tiles.py
@@ -14,22 +14,39 @@ import PIL.Image
 from constants import CHESSBOARDS_DIR, TILES_DIR, USE_GRAYSCALE
 from chessboard_image import get_chessboard_tiles
 
-def save_tiles(tiles, chessboard_img_path):
+OVERWRITE = False
+
+def _img_sub_dir(chessboard_img_path):
+    """ The sub-directory where the chessboard tiles will be saved
+    """
+    sub_dir = chessboard_img_path.split("/")[3]
+    return os.path.join(TILES_DIR, sub_dir)
+
+def _img_save_dir(chessboard_img_path):
+    """ The directory within the sub-directory that will contain the
+        tile images
+    """
+    img_filename_prefix = chessboard_img_path.split("/")[4][:-4]
+    return os.path.join(_img_sub_dir(chessboard_img_path), img_filename_prefix)
+
+def save_tiles(tiles, chessboard_img_path, overwrite=OVERWRITE):
     """ Saves all 64 tiles as 32x32 PNG files with this naming convention:
 
         a1_R.png (white rook on a1)
         d8_q.png (black queen on d8)
         c4_1.png (nothing on c4)
     """
-    img_dir = chessboard_img_path.split("/")[3]
-    output_dir = os.path.join(TILES_DIR, img_dir)
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    sub_dir = _img_sub_dir(chessboard_img_path)
+    if not os.path.exists(sub_dir):
+        os.makedirs(sub_dir)
     img_filename_prefix = chessboard_img_path.split("/")[4][:-4]
     # img_filename_prefix shows which piece is on which square:
     # RRqpBnNr-QKPkrQPK-PpbQnNB1-nRRBpNpk-Nqprrpqp-kKKbNBPP-kQnrpkrn-BKRqbbBp
-    img_save_dir = os.path.join(output_dir, img_filename_prefix)
+    img_save_dir = _img_save_dir(chessboard_img_path)
     print("\tSaving tiles to {}\n".format(img_save_dir))
+    if os.path.exists(img_save_dir) and not overwrite:
+        print("\tIgnoring existing {}\n".format(img_save_dir))
+        return
     if not os.path.exists(img_save_dir):
         os.makedirs(img_save_dir)
     piece_positions = img_filename_prefix.split('-')
@@ -49,9 +66,15 @@ def generate_tiles_from_all_chessboards():
     chessboard_img_filenames = glob("{}/*/*.png".format(CHESSBOARDS_DIR))
     num_chessboards = len(chessboard_img_filenames)
     num_success = 0
+    num_skipped = 0
     num_failed = 0
     for i, chessboard_img_path in enumerate(chessboard_img_filenames):
         print("%3d/%d %s" % (i + 1, num_chessboards, chessboard_img_path))
+        img_save_dir = _img_save_dir(chessboard_img_path)
+        if os.path.exists(img_save_dir) and not OVERWRITE:
+            print("\tIgnoring existing {}\n".format(img_save_dir))
+            num_skipped += 1
+            continue
         tiles = get_chessboard_tiles(chessboard_img_path, use_grayscale=USE_GRAYSCALE)
         if len(tiles) != 64:
             print("\t!! Expected 64 tiles. Got {}\n".format(len(tiles)))
@@ -60,8 +83,8 @@ def generate_tiles_from_all_chessboards():
         save_tiles(tiles, chessboard_img_path)
         num_success += 1
     print(
-        'Processed {} chessboard images ({} generated, {} failed)'.format(
-            num_chessboards, num_success, num_failed
+        'Processed {} chessboard images ({} generated, {} skipped, {} failed)'.format(
+            num_chessboards, num_success, num_skipped, num_failed
         )
     )
 
